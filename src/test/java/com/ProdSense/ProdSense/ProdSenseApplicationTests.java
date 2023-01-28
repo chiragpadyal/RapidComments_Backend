f27f8fd07c57bd880c7c285036a229b401f53877
chiragpadyal <freakstar03@gmail.com>
2023-01-28 20:29:35 +0530

    init: project

---
+++ src/test/java/com/ProdSense/ProdSense/ProdSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProdSense.ProdSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
