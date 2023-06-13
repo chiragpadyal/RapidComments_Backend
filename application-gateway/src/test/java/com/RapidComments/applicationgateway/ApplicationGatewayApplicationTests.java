8c54869cb30df71c74b527a7c92297b4f552ca15
chiragpadyal <freakstar03@gmail.com>
2023-06-13 19:34:30 +0530

    feat: added application gateway

---
+++ application-gateway/src/test/java/com/RapidComments/applicationgateway/ApplicationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RapidComments.applicationgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
